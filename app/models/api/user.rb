34409168999732e8c169b04a52856c7048791e0e
LP18 <amyjethra@gmail.com>
2020-12-10 13:36:01 +0530

    Assignment mongo

---
+++ app/models/api/user.rb
@@ -0,0 +1,7 @@
+class Api::User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :firstname, type: String
+  field :lastname, type: String
+  field :email, type: String
+end
---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Api::User.create(firstname:'albert',lastname:'einstein',email:'ae@relativity.com')
+
+Api::User.create(firstname:'marie',lastname:'curie',email:'ae@relativity.com')
+
+Api::User.create(firstname:'issac',lastname:'newton',email:'in@gravity.com')
+
+Api::User.create(firstname:'galileo',lastname:'galilei',email:'gg@astronomy.com')
+
+
+
+
+
+
+
+

aab7122f5657bb9be20465235d191e3d5863f819
LP18 <amyjethra@gmail.com>
2020-12-10 14:24:59 +0530

    added typehead search

--- app/controllers/api/typeheads_controller.rb
+++ app/controllers/api/typeheads_controller.rb
@@ -2,10 +2,11 @@ class Api::TypeheadsController < ApplicationController
     def index
         search = params[:query]
         
-        @users = Api::User.where("firstname LIKE ?", "%" + search + "%")
+        @users = Api::User.where(firstname: search).or(lastname: search).or(email: search)
 
         render json:@users
     end
+
 
 
   end

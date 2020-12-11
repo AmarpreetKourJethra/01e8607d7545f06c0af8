d560a8e7b9ebb95bda8462740c5c427d72592612
LP18 <amarpreetkourjethra@green-apex.com>
2020-12-11 14:48:39 +0530

    fixed minor regex match in typehead for mongo

--- app/controllers/api/typeheads_controller.rb
+++ app/controllers/api/typeheads_controller.rb
@@ -2,7 +2,8 @@ class Api::TypeheadsController < ApplicationController
     def index
         search = params[:query]
         
-        @users = Api::User.where(firstname: search).or(lastname: search).or(email: search)
+        @users = Api::User.where("firstname" => /.*#{search}.*/i).or("lastname" => /.*#{search}.*/i).or("email" => /.*#{search}.*/i)
+
 
         render json:@users
     end
